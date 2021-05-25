9fc94c406087bd16c178c2dc9430c9e30fdacbf7
kk488 <78923924+kk488@users.noreply.github.com>
2021-05-25 16:25:58 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -13,3 +13,15 @@ assert hypotenuse(12,5,False) == 13
 assert hypotenuse(5,3,True) == 4
 (2 points)
 """
+import math
+def hypotenuse(a,b,flag):
+    if flag==True:
+        if a>b:
+            c=float(math.sqrt(a**2-b**2))
+        elif a<b:
+            c=float(math.sqrt(b**2-a**2))
+        return c
+    elif flag==False:
+        c-float(math.sqrt(a**2+b**2))
+        return c
+        
